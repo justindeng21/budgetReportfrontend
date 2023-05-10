da5afa8c8a05acc7ceddefac1f587c3aad6da1cf
Justin Deng <justin.n.deng@gmail.com>
2023-05-10 00:25:27 -0600

    auto deploy

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -4,6 +4,17 @@ import './form.css'
 
 import logo from '../logo.svg';
 import '../App.css';
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer
+  } from "recharts";
+
 
 var domain = 'https://budgetreportapi.herokuapp.com'
 
@@ -34,6 +45,7 @@ const ExpenseForm = ()=>{
 
     return(
         <form onSubmit={(event) => event.preventDefault()} className='form'>
+            <h5 className='postCallHeading'>Report Expense</h5>
             <input type="text" className="basic-input" value = {expense} pattern="[0-9]*\.[0-9]{2}" placeholder='Expense' onChange={(e) => setExpense(e.target.value)}/>
             <input type="text" className="basic-input" maxLength={50} value = {transactionDescription} placeholder='Desc.' onChange={(e) => setDesc(e.target.value)}/>
             <button type='submit' className="submit" onClick={handleSubmit}>Submit</button>
@@ -64,6 +76,7 @@ const IncomeForm = ()=>{
 
     return(
         <form onSubmit={(event) => event.preventDefault()} className='form'>
+            <h5 className='postCallHeading'>Report Income</h5>
             <input type="text" className="basic-input" value = {income} pattern="[0-9]*\.[0-9]{2}" placeholder='income' onChange={(e) => setIncome(e.target.value)}/>
             <button type='submit' className="submit" onClick={handleSubmit}>Submit</button>
         </form>
@@ -101,6 +114,7 @@ export default class ReportingTool extends Component<{},transactionData>{
     }
 
     calcCurrentBalence(){
+
         var balance = this.state.budgetReport[0].income
         for(var i = 0; i< this.state.data.length; i++){
             balance -= this.state.data[i].expense
@@ -136,60 +150,97 @@ export default class ReportingTool extends Component<{},transactionData>{
         var transactions = this.state
         return(
 
-            <div className='reportingTool'>
-                <div className="heading"><h2>Monthly Expense Report<img src={logo} className="App-logo" alt="logo" /></h2></div>
 
-                
+            <div className="temp">
                 <div className="expenseContainer">
-                <h4>Api Calls</h4>
-                    <h5>Report Expense</h5>
+                <h5 className='postCallHeading'>Api Calls</h5>
+                    
                     <ExpenseForm></ExpenseForm>
-                    <h5>Report Income</h5>
+
                     <IncomeForm></IncomeForm>
                 </div>
 
-                <div className="budgetReportContainer">
-                <h3>Budget Report</h3>
-                    <table >
-                        <tbody>
-                        <tr>
-                            <td >Transaction Date</td>
-                            <td >Income</td>
-                            <td >Current Balence</td>
-                        </tr>
+            
 
-                        {transactions.budgetReport.map(data => 
-                        <tr key={data.id}>
-                            <td >{data.reportDate.split('T')[0].split('-')[1]+'/'+data.reportDate.split('T')[0].split('-')[2]}</td>
-                            <td >{data.income}</td>
-                            <td >{this.state.currentBalence.toFixed(2)}</td>
-                        </tr>)}
-                        </tbody>
-                    </table>
-                </div>
-    
-                <div className="reportContainer">
-                    <h3>Expenses</h3>
-                    <table >
-                        <tbody>
-                        <tr>
-                            <td >Transaction Date</td>
-                            <td >Expense</td>
-                            <td >Details</td>
-                        </tr>
-                        {transactions.data.map(data => 
-                        <tr key={data.id}>
-                            <td >{data.transactionDate.split('T')[0].split('-')[1]+'/'+data.transactionDate.split('T')[0].split('-')[2]}</td>
-                            <td >{data.expense}</td>
-                            <td >{data.transactionDescription}</td>
-                        </tr>)}
-                        
-                        </tbody>
-                    </table>
-        
-                </div>
-                <footer className='footer'></footer>
+                <div className='reportingTool'>
+                    <div className="heading"><h2>Monthly Expense Report</h2><img src={logo} className="App-logo" alt="logo" /></div>
+
+                    
+                    
+
+                    <div className="budgetReportContainer">
+                    <h3>Budget Report</h3>
+                        <table >
+                            <tbody>
+                            <tr>
+                                <td >Budget Date</td>
+                                <td >Budget</td>
+                                <td >Current Balence</td>
+                            </tr>
+
+                            {transactions.budgetReport.map(data => 
+                            <tr key={data.id} className='reportRow'>
+                                <td className='reportField'>{data.reportDate.split('T')[0].split('-')[1]+'/'+data.reportDate.split('T')[0].split('-')[2]}</td>
+                                <td className='reportField'>{data.income}</td>
+                                <td className='reportField'>{this.state.currentBalence.toFixed(2)}</td>
+                            </tr>)}
+                            </tbody>
+                        </table>
+                    </div>
+                    
+                    <div className="expenseReportContainer">
+                        <h3>Expenses</h3>
+                        <table >
+                            <tbody>
+                            <tr>
+                                <td >Transaction Date</td>
+                                <td >Expense</td>
+                                <td >Details</td>
+                            </tr>
+                            {transactions.data.map(data => 
+                            <tr key={data.id} className='expenseRow'>
+                                <td className='expenseField'>{data.transactionDate.split('T')[0].split('-')[1]+'/'+data.transactionDate.split('T')[0].split('-')[2]}</td>
+                                <td className='expenseField'>{data.expense}</td>
+                                <td className='expenseField'>{data.transactionDescription}</td>
+                            </tr>)}
                             
+                            </tbody>
+                        </table>
+            
+                    </div>
+                    <footer className='footer'><br /><br /></footer>
+                                
+                </div>
+
+                <div style={{ width: "30vw", 
+                  height: "30vh",
+                  backgroundColor: "#005F6B" ,borderRadius:"10px",margin:"10vh 10vh auto"}}>
+
+                <ResponsiveContainer width="100%" 
+                                    height="100%">
+                    <LineChart
+                    width={500}
+                    height={300}
+                    
+                    data={transactions.data}
+                    margin={{
+                        top: 40,
+                        right: 40,
+                        left: 40,
+                        bottom: 5,
+                    }}
+                    >
+                    <CartesianGrid />
+                    <YAxis tick={{ fill: 'white' }}/>
+                    <Tooltip/>
+                    <Legend />
+                    <Line type="monotone" dataKey="expense" stroke="white"  strokeWidth={3} dot={{ stroke: 'red'}} />
+                   
+                    </LineChart>
+                </ResponsiveContainer>
+                </div>
+
+
             </div>
         
         )
